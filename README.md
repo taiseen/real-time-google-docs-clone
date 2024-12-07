515c244c91534aa422888683309f4e00086585ac
taiseen <taiseen.cse@gmail.com>
2024-12-08 03:57:20 +0600

    feat: project init ✨

---
+++ README.md
@@ -0,0 +1,10 @@
+> 08 - December - 2024
+
+## Real Time Google Doc Clone
+
+* npx create-next-app@15.0.3
+* npx shadcn@2.1.6 init
+
+```
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default Home;
