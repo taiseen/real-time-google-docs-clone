f01c76dd1495b96881ccc5f4baab1c25f948ba45
taiseen <taiseen.cse@gmail.com>
2024-12-27 22:17:08 +0600

    style: typo fix in avatar

--- src/app/document/[docId]/avatar.tsx
+++ src/app/document/[docId]/avatar.tsx
@@ -45,11 +45,11 @@ const Avatar = ({ src, name }: AvatarProps) => {
     <div
       style={{ width: AVATAR_SIZE, height: AVATAR_SIZE }}
       className="group -ml-2 flex shrink-0 place-content-center relative border-4
-    border-white rounded-full [bg-gray-400"
+    border-white rounded-full bg-gray-400"
     >
       <div
         className="opacity-0 group-hover:opacity-100 absolute top-full py-1 px-2 text-white
-text-xs rounded-lg mt-2.5 z-10 Obg-black whitespace-nowrap transition-opacity"
+text-xs rounded-lg mt-2.5 z-10 bg-black whitespace-nowrap transition-opacity"
       >
         {name}
       </div>
